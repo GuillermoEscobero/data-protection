c933893de84a0ee8542e0378974ec3159c9cda7d
Guillermo Escobero <100346060@alumnos.uc3m.es>
2020-10-04 21:19:31 +0200

    Header comments

--- src/main/java/lab1/SymmetricEncryption.java
+++ src/main/java/lab1/SymmetricEncryption.java
@@ -1,3 +1,14 @@
+/**
+ * @Author: Guillermo Escobero, Alvaro Santos
+ * @Date:   04-10-2020
+ * @Project: Data Protection Lab 2
+ * @Filename: SymmetricEncryption.java
+ * @Last modified by:   Guillermo Escobero, Alvaro Santos
+ * @Last modified time: 04-10-2020
+ */
+
+
+
 package main.java.lab1;
 
 import java.io.FileDescriptor;
