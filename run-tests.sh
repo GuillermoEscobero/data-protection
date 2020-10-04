dda5e6065d57fc8a300a3b48061947e55d9f3dad
Guillermo Escobero <100346060@alumnos.uc3m.es>
2020-10-04 23:41:13 +0200

    Added run-tests.sh

---
+++ run-tests.sh
@@ -0,0 +1,12 @@
+# Enter in classpath
+cd src/
+
+# Compile java files
+javac main/java/lab1/SymmetricEncryption.java
+javac main/java/lab1/SymmetricCipher.java
+
+# Compile tests
+javac test/java/lab1/TestSymmetricCipher.java
+
+# Execute tests
+java test/java/lab1/TestSymmetricCipher
