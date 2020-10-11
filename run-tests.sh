4a02f28dcb52f783805a1f7ff1709f96d2a3cfea
Guillermo Escobero <100346060@alumnos.uc3m.es>
2020-10-11 15:48:03 +0200

    Added lab2 files to run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -5,8 +5,14 @@ cd src/
 javac main/java/lab1/SymmetricEncryption.java
 javac main/java/lab1/SymmetricCipher.java
 
+javac main/java/lab2/RSALibrary.java
+
 # Compile tests
 javac test/java/lab1/TestSymmetricCipher.java
 
+javac test/java/lab2/TestRSALibrary.java
+
 # Execute tests
 java test/java/lab1/TestSymmetricCipher
+
+java test/java/lab2/TestRSALibrary
